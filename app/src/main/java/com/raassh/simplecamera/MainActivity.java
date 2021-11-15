a93247c7168df467bb985afd9c638a8bf354a06a
Junaedi Akbar <juned.akb@gmail.com>
2021-11-15 19:29:26 +0800

    edit tampilan awal

--- app/src/main/java/com/raassh/simplecamera/MainActivity.java
+++ app/src/main/java/com/raassh/simplecamera/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -30,7 +31,7 @@ import java.io.ByteArrayOutputStream;
 public class MainActivity extends AppCompatActivity {
     Button btnCam;
     Button btnList;
-    Button btnSend;
+    ImageButton btnSend;
     ImageView imgView;
     EditText etServer;
 
@@ -70,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Bitmap bm = ((BitmapDrawable)imgView.getDrawable()).getBitmap();
-
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
                 byte[] byteArray = stream.toByteArray();
